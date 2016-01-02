3fbe28a0c415fea2e34c07254be65736afd31415
rafi revach <rafi revach>
2016-01-02 14:38:54 +0200

    Finish orders view

--- public/controllers/ordersController.js
+++ public/controllers/ordersController.js
@@ -1,26 +1,55 @@
 angular.module('BookStoreApp').controller('ordersController', ['$scope', '$http', '$routeParams',
                                           function($scope, $http, $routeParams) {
-      $scope.orders = [
-          {
-              date: '1-1-2016',
-              bookId: '333',
-              customerId: '1'
-          },
-          {
-              date: '1-1-2016',
-              bookId: '444',
-              customerId: '1'
-          },
-          {
-              date: '1-1-2016',
-              bookId: '555',
-              customerId: '1'
-          },
-          {
-              date: '1-1-2016',
-              bookId: '666',
-              customerId: '1'
-          }
-      ];
+      $scope.orders = [];
 
+      // Get all orders
+      var allOrders = function() {
+          $http.get('api/orders/')
+              .then(function(response) {
+                  var orders = response.data;
+
+                  // Get the books ids of the books in the orders
+                  var booksIds = [];
+                  orders.forEach(function(order){
+                      if (booksIds.indexOf(order.bookId) == -1)
+                      {
+                          booksIds.push(order.bookId);
+                      }
+                  });
+
+                  // Get the books names from the ids
+                  $http.get('api/books/booksNames/' + booksIds)
+                      .then(function(response){
+
+                          var bookNames = response.data;
+
+                          orders.forEach(function(order){
+                              // Get the book name
+                              var bookName = '';
+                              bookNames.forEach(function(book){
+                                  if (book.id == order.bookId){
+                                      bookName = book.name
+                                  }
+                              });
+
+                              var orderWithName =
+                              {
+                                  date: order.date,
+                                  bookName: bookName,
+                                  customerId: order.customerId
+                              };
+
+                              $scope.orders.push(orderWithName);
+                          })
+                      })
+                      .catch(function(err) {
+                          console.error('Error', err);
+                      });
+              })
+              .catch(function(err) {
+                  console.error('Error', err);
+              });
+      };
+
+      allOrders();
 }]);
